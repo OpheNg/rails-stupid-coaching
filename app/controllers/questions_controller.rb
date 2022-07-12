4e89f34d76d78631320fad17544a0941361d664b
OpheNg <ophe.ng@live.fr>
2022-07-12 18:35:22 +0200

    answers OK

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,25 @@
 class QuestionsController < ApplicationController
+
+  # ans_work = 'Great!'
+  # ans_question = 'Silly question, get dressed and go to work!'
+  # ans_else = "I don't care, get dressed and go to work!"
+
   def ask
   end
 
   def answer
+    @question = params[:question]
+    @answer = coach(@question)
+  end
+
+  def coach(smtg)
+    work = 'I am going to work'
+    if smtg == work
+      'Great!'
+    elsif smtg.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
   end
 end
